84910488e27960074d40b48f2789f3e5c68b2106
Lucas da Silva <sklucassilva@gmail.com>
2025-03-12 17:55:32 -0300

    feat: remove lógica de busca de dados de volumetria do arquivo api.ts e implementa chamada diretamente no componente de relatório, adiciona funcionalidade para resetar gravação com feedback ao usuário

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,7 +8,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       data-slot="input"
       className={cn(
         "border border-gray-300 rounded-lg px-4 py-2 text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-200 transition duration-150 ease-in-out",
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-10 w-full min-w-0 rounded-md bg-white shadow-sm",
+        "file:text-foreground placeholder:text-muted-foreground selection:bg-blue-500 selection:text-white flex h-10 w-full min-w-0 rounded-md bg-white shadow-sm",
         "file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
         className
       )}
