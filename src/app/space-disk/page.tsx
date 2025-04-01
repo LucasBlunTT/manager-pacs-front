187b303f621e3166fabe9670af50271ca18ae390
Lucas da Silva <sklucassilva@gmail.com>
2025-04-01 10:46:11 -0300

    feat: adiciona loading ao componente SpaceDisk e exibe nome do disco no gráfico

--- src/app/space-disk/page.tsx
+++ src/app/space-disk/page.tsx
@@ -3,35 +3,55 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import CustomGauge from '@/components/SpaceDiscChart';
+import Loading from '@/components/Loading';
 
 interface DiskSpace {
   total: number;
   free: number;
 }
 
-
 export default function SpaceDisk() {
   const [diskSpace, setDiskSpace] = useState<DiskSpace | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [discName, setDiscName] = useState<string>('');
 
   useEffect(() => {
     async function fetchDiskSpace() {
       try {
+        setLoading(true);
         const result = await axios.get('http://localhost:3333/api/disco-ativo');
-        setDiskSpace(result.data); 
+        setDiskSpace(result.data);
+        setDiscName(result.data.discName);
+
+        if (result.data.rawResult && result.data.rawResult.length > 0) {
+          setDiscName(result.data.rawResult[0].no_localstore);
+        }
       } catch (error) {
         console.error('Erro ao buscar espaço em disco:', error);
+      } finally {
+        setLoading(false);
       }
     }
 
+    // Chamada inicial
     fetchDiskSpace();
+
+    // Configurando o intervalo para 30 minutos
+    const interval = setInterval(fetchDiskSpace, 1800000);
+
+    // Limpando o intervalo ao desmontar o componente
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="w-screen h-screen grid items-center justify-center gap-4 p-10 grid-cols-4 max-sm:grid-cols-1 max-lg:grid-cols-2 lg:grid-cols-4">
-      {diskSpace && (
-        <>
-          <CustomGauge spaceTotal={diskSpace.total} spaceFree={diskSpace.free} />
-        </>
+      {loading ? (
+        <Loading />
+      ) : (
+        diskSpace && (
+          <CustomGauge spaceTotal={diskSpace.total} spaceFree={diskSpace.free} discName={discName} />
+        )
+        
       )}
     </div>
   );
--- src/components/SpaceDiscChart/index.tsx
+++ src/components/SpaceDiscChart/index.tsx
@@ -1,30 +1,42 @@
+'use client'
+
 import React from 'react'
 import { Gauge, gaugeClasses } from '@mui/x-charts/Gauge';
 
 interface CustomGaugeProps {
   spaceTotal: number
   spaceFree: number
+  discName?: string
 }
 
-export default function CustomGauge({ spaceTotal, spaceFree }: CustomGaugeProps) {
+export default function CustomGauge({ spaceTotal, spaceFree, discName }: CustomGaugeProps) {
   const spaceUsed = spaceTotal - spaceFree;
   const percentageUsed = (spaceUsed / spaceTotal) * 100;
 
   return (
-    <Gauge
-      value={percentageUsed}  
+    <div className="flex flex-col items-center justify-center bg-white shadow-lg rounded-lg p-4">
+      <p className='text-center text-lg font-bold'>{discName}</p>
+      <Gauge
+      width={300}
+      height={300}
+      value={percentageUsed}
       startAngle={-110}
       endAngle={110}
       sx={{
         [`& .${gaugeClasses.valueText}`]: {
-          fontSize: 20,
-          fontWeight: 'bold',  
-          transform: 'translate(0px, 0px)',
+        fontSize: 20,
+        fontWeight: 'bold',
+        transform: 'translate(0px, 0px)',
+        },
+        [`& .${gaugeClasses.valueArc}`]: {
+        fill: `${percentageUsed > 80 ? '#FF0000' : percentageUsed > 50 ? '#FFA500' : '#008000'}`,
+        },
+        [`& .${gaugeClasses.referenceArc}`]: {
+        fill: '#D3D3D3',
         },
       }}
-      text={
-        () => `${spaceUsed} GB / ${spaceTotal} GB`
-      }
-    />
+      text={() => `${spaceUsed} GB / ${spaceTotal} GB`}
+      />
+    </div>
   )
 }
